0caeb14af00972f9bd5d7ca28d137996924afbb3
Anniina Aarnio <aarnipex@student.jyu.fi>
2022-11-02 19:17:29 +0200

    värinvaihto palkissa onnistuu suunnilleen oikein

--- vt4.js
+++ vt4.js
@@ -2,24 +2,25 @@
 //@ts-check 
 
 window.onload = function () {
-    lisaaDefs();
+    lisaaDefs(2);
+    lisaaDefs(4);
     for (let i = 0; i < 10; i++) {
         teePalkki(i);
     }
 };
 
-function lisaaDefs() {
+function lisaaDefs(versio) {
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
     let defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
     let linearGradient = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
-    linearGradient.setAttribute("id","Gradient1");
+    linearGradient.setAttribute("id","Gradient" + versio);
     linearGradient.setAttribute("gradientTransform", "rotate(90)");
     let stop1 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
     let stop2 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
     let stop3 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
     stop1.setAttribute("class", "stop1");
-    stop2.setAttribute("class", "stop2");
+    stop2.setAttribute("class", "stop" + versio);
     stop3.setAttribute("class", "stop3");
     stop1.setAttribute("offset", "0%");
     stop2.setAttribute("offset", "50%");
@@ -44,19 +45,18 @@ function teePalkki(delay) {
     svg.setAttribute("class", "hbar");
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
     svg.setAttribute("version", "1.1");
-    svg.setAttribute("width", "100%");
-    svg.setAttribute("height", "50");
+    svg.setAttribute("width", "100vw");
+    svg.setAttribute("height", "30");
 
     let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     rect.setAttribute("x", "0");
     rect.setAttribute("y", "0");
     rect.setAttribute("width", "100%");
     rect.setAttribute("height", "100%");
-    rect.setAttribute("fill", "url(#Gradient1)");
+
 
     svg.appendChild(rect);
     svg.style.animationDelay = String(delay * 100) + "ms";
-    svg.style.transitionTimingFunction = "linear";
     document.body.appendChild(svg);
 
 }
